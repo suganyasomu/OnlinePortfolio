694766c00b1f8af14c720f4b6735abef903d3642
suganya somu <sweetsugan12@gmail.com>
2020-09-08 01:27:32 -0700

    updated files

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
 $(document).ready(function () {
+  $(".image").click(function () {
+    var getId = this.id;
+    console.log(getId);
 
-
-    $('.image').click(function () {
-
-        var getId = this.id;
-        console.log(getId);
-
-        if (getId === "weather") {
-            window.location.href = "projectDetails.html";
-            console.log("if condition is called");
-            // $("#jobSearchProj").css("display", "none");
-            // $("#eventProj").css("display", "none");
-            $("#weatherDisplay").css("display", "block");
-
-
-        } else if (getId === "jobSearch") {
-
-            $("#jobSearchProj").css("display", "inline-block");
-
-            // $("#eventProj").css("display", "none");
-            // $("#weatherDisplay").css("display", "none");
-
-        } else {
-
-            // $("#jobSearchProj").css("display", "none");
-            $("#eventProj").css("display", "inline-block");
-            // $("#weatherDisplay").css("display", "none");
-
-        }
-
-    });
+    if (getId === "weather") {
+      window.location.href = "weather.html";
+    } else if (getId === "jobSearch") {
+      window.location.href = "jobSearch.html";
+    } else {
+      window.location.href = "dailyscheduler.html";
+    }
+  });
 });
