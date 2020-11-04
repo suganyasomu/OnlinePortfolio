560a50332bb2de2e6fc705a06fe3effb9650680f
suganya somu <sweetsugan12@gmail.com>
2020-11-03 23:52:41 -0800

    updated the switch statement

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ $(document).ready(function () {
     var getId = this.id;
     console.log(getId);
 switch(getId){
-  case weather:
+  case 'weather':
      window.location.href = "weather.html";
   break;
-  case jobSearch: 
+  case 'jobSearch': 
   window.location.href = "jobSearch.html";
   break;
-  case dailyEvent: 
+  case 'dailyEvent': 
   window.location.href = "dailyScheduler.html";
   break;
-  case burger: 
+  case 'burger': 
   window.location.href = "burgerApp.html";
   break;
-default:
+ default:
   window.location.href = "foodReview.html";
-};
+ };
   });
 });
